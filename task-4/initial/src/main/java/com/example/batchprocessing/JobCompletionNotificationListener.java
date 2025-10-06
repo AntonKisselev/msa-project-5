059b0351b0e4ad625bba266cf8f6beeb1b581cec
Anton <31483710+AntonKisselev@users.noreply.github.com>
2025-10-06 13:11:37 +0300

    task 4

--- task-4/initial/src/main/java/com/example/batchprocessing/JobCompletionNotificationListener.java
+++ task-4/initial/src/main/java/com/example/batchprocessing/JobCompletionNotificationListener.java
@@ -6,25 +6,37 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
-import org.springframework.jdbc.core.DataClassRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JobCompletionNotificationListener implements JobExecutionListener {
 
-	private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+    private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-	private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
-	public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
+    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-	@Override
-	public void afterJob(JobExecution jobExecution) {
-		if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
-			// todo
-		}
-	}
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+            log.info("!!! JOB FINISHED! Time to verify the results");
+
+            String sql = "SELECT productId, productSku, productName, productAmount, productData FROM products";
+
+            jdbcTemplate.query(sql, (rs, rowNum) -> new Product(
+                rs.getLong("productId"),
+                rs.getLong("productSku"),
+                rs.getString("productName"),
+                rs.getLong("productAmount"),
+                rs.getString("productData")
+            )).forEach(product -> log.info("Found <{}> in the database.", product));
+
+            log.info("Total products processed: {}",
+                jdbcTemplate.queryForObject("SELECT COUNT(*) FROM products", Integer.class));
+        }
+    }
 }
